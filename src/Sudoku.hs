9d5fd945981c171e05e7fdfe3318540890838f5b
Christof Schramm <schrammc@Christofs-MacBook-Pro.fritz.box>
2020-02-08 17:34:54 +0100

    Add pretty printing function for sudoku

--- src/Sudoku.hs
+++ src/Sudoku.hs
@@ -1,14 +1,31 @@
+{-# LANGUAGE MultiWayIf #-}
 module Sudoku where
 
-import Data.Char (isDigit)
-import Data.IntMap (IntMap)
+import           Data.Char (isDigit)
+import           Data.IntMap (IntMap)
 import qualified Data.IntMap as IM
+import           Data.Maybe
 
 newtype Sudoku = Sudoku (IntMap Int)
                  deriving (Show, Eq)
 
 exampleSudoku :: String
 exampleSudoku = "769.....8....5....8.....34.....34..7.8...7..........9.63..78.....5.69......2.54.."
+
+printBoard :: Sudoku -> IO ()
+printBoard = putStrLn . formatBoard
+
+formatBoard :: Sudoku -> String
+formatBoard (Sudoku mp) = go [0 .. 80]
+  where
+    go [] = ""
+    go (n:ns) =
+      let val = fromMaybe "." $ fmap show $ IM.lookup n mp
+          rest = val ++ go ns
+      in if | n > 0 && n `mod` 27 == 0 -> "\n---+---+---\n" ++ rest
+            | n > 0 && n `mod` 9 == 0 ->"\n" ++ rest
+            | n > 0 && n `mod` 3 == 0 -> "|" ++ rest
+            | otherwise -> rest
 
 parseSudoku :: String -> Maybe Sudoku
 parseSudoku xs
