9d6265b544e8c262fa6aff2604fa11d1e91a0fd8
Christof Schramm <schrammc@Christofs-MBP.fritz.box>
2020-02-09 11:19:12 +0100

    Add a bit of comment

--- src/Sudoku.hs
+++ src/Sudoku.hs
@@ -16,11 +16,13 @@ exampleSudoku = "1...5.8.3..817..544.2..36.....534...36.7..............2....7...
 printBoard :: Sudoku -> IO ()
 printBoard = putStrLn . formatBoard
 
+-- | Render to a string as given by https://qqwing.com/generate.html
 formatBoardOneLine :: Sudoku -> String
 formatBoardOneLine (Sudoku mp) = concat $ do
   i <- [0 .. 80]
   return $ fromMaybe "." $ fmap show $ IM.lookup i mp
 
+-- | Render to a nice human-readable formulation
 formatBoard :: Sudoku -> String
 formatBoard (Sudoku mp) = go [0 .. 80]
   where
@@ -37,19 +39,24 @@ formatBoard (Sudoku mp) = go [0 .. 80]
 freePoints :: Sudoku -> [Int]
 freePoints (Sudoku mp) = filter (\x -> not (IM.member x mp)) [0 ..80]
 
+-- | Get all possible solutions of this Sudoku
 solutions :: Sudoku -> [Sudoku]
 solutions s = solvePoints s (freePoints s)
 
+-- | Get all Sudokus that have all given positions solved
 solvePoints :: Sudoku -> [Int] -> [Sudoku]
 solvePoints sudoku [] = [sudoku]
 solvePoints sudoku (p:ps) = do
   sudoku' <- pointSolutions sudoku p
   solvePoints sudoku' ps
 
+-- | Get all Sudokus with valid solutions for the given position
 pointSolutions :: Sudoku -> Int -> [Sudoku]
 pointSolutions sudoku@(Sudoku mp) field =
   fmap (\value -> Sudoku (IM.insert field value mp)) (possibleValues sudoku field)
 
+-- | List of values that can be filled into the given position, empty if field
+-- already occupied
 possibleValues :: Sudoku -> Int -> [Int]
 possibleValues sudoku@(Sudoku mp) field
   | Just k <- IM.lookup field mp = []
@@ -61,7 +68,7 @@ possibleValues sudoku@(Sudoku mp) field
              , valuesInColumn sudoku field
              , valuesInBox sudoku field]
 
--- Check if a sudoku puzzle is solved
+-- | Check if a sudoku puzzle is solved
 isSolved :: Sudoku -> Bool
 isSolved s = rowsOK && columnsOK && boxesOK
   where
@@ -71,6 +78,7 @@ isSolved s = rowsOK && columnsOK && boxesOK
     boxTopLeftPoints = concat $ take 3 $ iterate (fmap (+27)) [0, 3, 6]
     allNumbers = IS.fromList [1..9]
 
+-- | Values in the same 3 x 3 box as the given position
 valuesInBox :: Sudoku -> Int -> [Int]
 valuesInBox (Sudoku mp) pos = do
   let currentCol = pos `mod` 9
@@ -80,18 +88,21 @@ valuesInBox (Sudoku mp) pos = do
   let k = row * 9 + col
   maybe [] (:[]) (IM.lookup k mp)
 
+-- | Values in the same row as the given position
 valuesInRow :: Sudoku -> Int -> [Int]
 valuesInRow (Sudoku mp) pos = do
   let rowStart = pos - (pos `mod` 9)
   k <- [rowStart .. rowStart + 8]
   maybe [] (:[]) (IM.lookup k mp)
 
+-- | Values in the same column as the given position
 valuesInColumn :: Sudoku -> Int -> [Int]
 valuesInColumn (Sudoku mp) pos = do
   row <- [0 .. 8]
   let k = (row * 9) + (pos `mod` 9)
   maybe [] (:[]) (IM.lookup k mp)
 
+-- | Parse a one line sudoku as output by: https://qqwing.com/generate.html
 parseSudoku :: String -> Maybe Sudoku
 parseSudoku xs
   | length xs /= 81 && any (not . validChar) xs = Nothing
